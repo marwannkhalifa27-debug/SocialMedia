401ec55db749a22d1642419c09418115b601cd49
Marwan Mostafa Khalifa <marwannkhalifa.27@gmail.com>
2026-09-26 15:12:40 +0300

    fixes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,10 @@
 
 
 import { Router } from "express";
+import { getUserData } from "./user.service.js";
+import { authenticate } from "../../common/middleware/auth.middleware.js";
 const userRouter = Router()
 
+userRouter.get("/me", authenticate, getUserData)
 
 export default userRouter
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,5 @@
 import { userModel } from "../../DB/models/user.model.js"
-
+import type { Request, Response, NextFunction } from "express"
 
 export const findUserByEmail = async (email: string) => {
     return await userModel
@@ -9,4 +9,17 @@ export const findUserByEmail = async (email: string) => {
 
 export const findUserByUsername = async (username: string) => {
     return await userModel.findOne({ username })
+}
+export const findUserById = async (id: string) => {
+    return await userModel.findById(id)
+}
+
+export const getUserData = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const user = await findUserById(req.user!.id)
+        if (!user) return res.status(404).json({ message: "User not found" })
+        return res.status(200).json({ data: user })
+    } catch (error) {
+        next(error)
+    }
 }
