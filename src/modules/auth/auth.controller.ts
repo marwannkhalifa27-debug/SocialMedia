8f01d79300c4763fba477537459c8c35986a4b1e
Marwan Mostafa Khalifa <marwannkhalifa.27@gmail.com>
2026-09-26 13:40:42 +0300

    fixes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { login, register } from "./auth.service.js";
 import { validate } from "../../common/middleware/validation.middleware.js";
 import { loginSchema, registerSchema } from "./auth.validation.js";
+import { authenticate } from "../../common/middleware/auth.middleware.js";
 const authRouter = Router()
 
-authRouter.post("/register", validate(registerSchema), register)
-authRouter.post("/login", validate(loginSchema), login)
+authRouter.post("/register", authenticate, validate(registerSchema), register)
+authRouter.post("/login", authenticate, validate(loginSchema), login)
 
 
 export default authRouter
